cb5c0c63e1d633814ea7b98c1a8aedda860cf22a
Stephen Martin <stephen@martintechlabs.com>
2013-04-22 14:57:11 -0400

    using auth token instead of http auth

--- lib/phonegap-api/base.rb
+++ lib/phonegap-api/base.rb
@@ -8,32 +8,32 @@ module Phonegap
     follow_redirects false
     format :json
 
-    def initialize(*auth)
-      if auth != []
-        @auth = {:basic_auth => auth}
-      else
-        @auth = {:basic_auth => YAML.load_file("config/phonegap.yml")}
-      end
+    def initialize(token)
+      @auth_token = token
+    end
+
+    def auth_token_qs
+      "?auth_token=#{@auth_token}"
     end
     
     def get(url)
       puts @auth.inspect
-      output = self.class.get(url, @auth)
+      output = self.class.get(url + auth_token_qs)
       check_response!(output).parsed_response
     end
     
     def post(url, body)
-      output = self.class.post(url, @auth.merge!({:body =>{:data => body}}))
+      output = self.class.post(url + auth_token_qs, {:body =>{:data => body}})
       check_response!(output).parsed_response
     end
     
     def put(url, body)
-      output = self.class.put(url, @auth.merge!({:body =>{:data => body}}))
+      output = self.class.put(url + auth_token_qs, {:body =>{:data => body}})
       check_response!(output).parsed_response
     end
     
     def delete(url)
-      output = self.class.delete(url, @auth)
+      output = self.class.delete(url + auth_token_qs)
       check_response!(output).parsed_response
     end
     
